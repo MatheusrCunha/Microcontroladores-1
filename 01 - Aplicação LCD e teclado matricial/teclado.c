443ddc4deb039c5402662b75f94f611b6321fce6
MatheusrCunha <49212881+MatheusrCunha@users.noreply.github.com>
2020-03-10 12:28:02 -0300

    teste
    teste

---
+++ 01 - Aplicação LCD e teclado matricial/teclado.c
@@ -0,0 +1,18 @@
+
+#include "lcd.h"
+#include "teclado.h"
+#include "gpio.h"
+
+
+const unsigned char teclado[4][4] PROGMEM = {{'1', '2', '3', 'F'},
+                                              {'4', '5', '6', 'E'},
+                                              {'7', '8', '9', 'D'},
+                                              {'A', '0', 'B', 'C'}};
+
+
+
+
+int main()
+{
+
+}
---
+++ 01 - Aplicação LCD e teclado matricial/teclado.h
@@ -0,0 +1,14 @@
+#ifndef _TECLADO_H #define _TECLADO_H
+
+#include "def_principais.h"
+
+#define LINHA PIND   //registrador para a leitura das linhas #define COLUNA
+PORTD  //registrador para a escrita nas colunas
+//protótipo da função unsigned char ler_teclado();
+
+
+unsigned char ler_teclado();
+
+
+#endif
+
